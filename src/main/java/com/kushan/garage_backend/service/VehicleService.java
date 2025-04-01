71eacb99bcc708e83609765d0b42b7220e4b77c9
Kushan <kushansr7@gmail.com>
2025-04-01 21:33:19 +0530

    fix some issues

--- src/main/java/com/kushan/garage_backend/service/VehicleService.java
+++ src/main/java/com/kushan/garage_backend/service/VehicleService.java
@@ -4,8 +4,12 @@ package com.kushan.garage_backend.service;
 import com.kushan.garage_backend.entity.Vehicle;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VehicleService {
     Vehicle addVehicle(Vehicle vehicle);
     List<Vehicle> getVehiclesByCustomerId(Long customerId);
+    Vehicle updateVehicle(String vehicleNumber, Vehicle updatedVehicle);
+    boolean deleteVehicle(String vehicleNumber);
+
 }
--- src/main/java/com/kushan/garage_backend/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/kushan/garage_backend/service/impl/VehicleServiceImpl.java
@@ -6,6 +6,7 @@ import com.kushan.garage_backend.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
@@ -25,5 +26,27 @@ public class VehicleServiceImpl implements VehicleService {
     @Override
     public List<Vehicle> getVehiclesByCustomerId(Long customerId) {
         return vehicleRepository.findByCustomer_Id(customerId);
+    }
+
+    @Override
+    public Vehicle updateVehicle(String vehicleNumber, Vehicle updatedVehicle) {
+        return vehicleRepository.findById(vehicleNumber).map(vehicle -> {
+            vehicle.setColor(updatedVehicle.getColor());
+            vehicle.setCurrentRange(updatedVehicle.getCurrentRange());
+            vehicle.setDescription(updatedVehicle.getDescription());
+            vehicle.setFuelType(updatedVehicle.getFuelType());
+            vehicle.setModel(updatedVehicle.getModel());
+            vehicle.setYear(updatedVehicle.getYear());
+            return vehicleRepository.save(vehicle);
+        }).orElse(null);
+    }
+
+    @Override
+    public boolean deleteVehicle(String vehicleNumber) {
+        if (vehicleRepository.existsById(vehicleNumber)) {
+            vehicleRepository.deleteById(vehicleNumber);
+            return true;
+        }
+        return false;
     }
 }
